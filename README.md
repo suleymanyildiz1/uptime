d6034cd7d9c8664760254e2391a250fd5104681c
Glitch (uneven-seemly-shamrock) <none>
2020-08-06 14:45:37 +0000

    🌲👬 Checkpoint ./index.ejs:1947894/326:2929320/220 ./server.js:1947894/4708 ./README.md:2929320/804 ./src/pages/index.ejs:1947894/12685 ./src/pages/add.ejs:1947894/3751

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Uptime system
+### info
+`*` Apahce licence
+`*` [!]()
+`*` <img style="				width: 100%;
+				height: 100%;" src="https://cdn.discordapp.com/attachments/723279024050405427/740943260289335407/Asuna_Yuvarlak.png">
+
+
+
+### Apache licence
+`-` We are a license holder, if any ownership is something like saying I did it, the highest action will be taken!
--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const client = new discord.Client({ disableEveryone: true });
 client.login(process.env.TOKEN);
 const fetch = require("node-fetch");
 const fs = require("fs");
-/*const hook = new discord.WebhookClient(
-  "737669686883516498",
-  "X5vV9dX08B2GLLrE7WlSrcOhd6BmDBopJno9jAEO4ystn-58JfGOc_XJKGBjN0w14L3e"
-);*/
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
@@ -28,7 +24,7 @@ const session = require("express-session");
 const LevelStore = require("level-session-store")(session);
 const Strategy = require("passport-discord").Strategy;
 
-const templateDir = path.resolve(__dirname + `/`); // SITE DOSYA KONTROL
+  const templateDir = path.resolve(__dirname + `/src/pages/`); 
 
 app.locals.domain = process.env.PROJECT_DOMAIN;
 
@@ -58,7 +54,6 @@ setInterval(() => {
     }
   });
   let zaman = new Date();
-  /* hook.send(`Pong! Tüm linkler'e izleme gönderildi zaman; ${zaman}`)*/
   console.log("Pong! Requests sent");
 }, 60000);
 
@@ -70,9 +65,7 @@ client.on("ready", () => {
 
 
 client.on("ready", () => {
-  client.user.setActivity(
-    `Site kodlanıyor by cenap / uptime system by mertbhey`
-  );
+  client.user.setActivity(`by Alfonzo#1921 and cenap#4160`);
   passport.use(
     new Strategy(
       {
@@ -158,7 +151,7 @@ linkss = linkA
 
   app.get("/autherror", (req, res) => {
     res.send(
-      "Kardeşim kardeşim bağlantı hatası verdi diyor sistem geri gidip tekrar giriş yaparmısın"
+      "Auth Error!"
     );
   });
 
@@ -181,140 +174,34 @@ linkss = linkA
   app.get("/add", checkAuth, (req, res) => {
     renderTemplate(res, req, "add.ejs");
   });
-  app.get("/404", (req, res) => {
-    renderTemplate(res, req, "404.html");
+  app.get("/info", (req, res) => {
+    renderTemplate(res, req, "info.ejs");
+  });
+    app.get("/profile", (req, res) => {
+    renderTemplate(res, req, "profile.ejs");
   });
    
   app.post("/add", checkAuth, (req, res) => {
     let ayar = req.body;
     let link = ayar["link"];
-    if (!ayar["link"]) return res.send("Link'i doldurmadın");
+    if (!ayar["link"]) return res.send("You didn't fill out the link!");
 
-    /*     let ekleyen = "";
-      let dict =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-      for (var i = 0; i < 18; i++) {
-        ekleyen =
-          ekleyen + dict.charAt(Math.floor(Math.random() * dict.length));
-      }*/
  if(db.get("linkler").map(z => z.url).includes(link)) {
-      return res.send("Kardeş zaten var ne ekliyip sistemi zorlarlıştırcan");
+      return res.send("Already in the system!");
     } else {
       db.push("linkler", { url: link, owner: req.user.id });
-      res.send("eklendi " + req.user.id);
+      res.send("Added " + req.user.id);
     }
   });
 
   const listener = app.listen(process.env.PORT, () => {
-    console.log("Panel şu portla başlatıldı:" + listener.address().port);
+    console.log("Port:" + listener.address().port);
   });
-  console.log(`Logined`);
+  console.log(`Logined!`);
 });
-//console.log(db.get("linkler").map(z => z.url).includes("https://thyke-.glitch.me/")
-/*client.on("message", message => {
-  if(message.author.bot) return;
-  var spl = message.content.split(" ");
-  if(spl[0] == "u.add") {
-  var link = spl[1]
-  fetch(link).then(() => {
-    if(db.get("linkler").map(z => z.url).includes(link)) return message.channel.send("<:asuna_no:732219380795965471> Already Available!")
-    message.channel.send("<:asuna_yes:732219381085503529> Successful!");
-    db.push("linkler", { url: link, owner: message.author.id})
-  }).catch(e => {
-    return message.channel.send("<:asuna_no:732219380795965471> " + e)
-  })
-  }
-})
 
-
-client.on("message", message => {
-  if(message.author.bot) return;
-  var spl = message.content.split(" ");
-  if(spl[0] == "u.say") {
-  var link = spl[1]
- message.channel.send(`${db.get("linkler").length} / ${client.guilds.size}`)
-}})
-
-
-*/
 const Discord = require("discord.js");
 
-/*client.on("message", message => {
-  if(message.author.bot) return;
-    var spl = message.content.split(" ");
-  if(spl[0] == "u.help") {
-let embed = new Discord.RichEmbed()
-.setColor('#4ca74c')
-.addField(`Uptime Bot v1.0 Help`, `Includes a system that keeps uptime bot glitch sites open 24/7. The links in the system operate 24/7 without any maintenance.`)
-.addField(`General Commands`,`
-
-\`u.help\` - Displays the help menu.
-\`u.add\` - Adds the link you specified to the system.
-\`u.say\` - It shows the number of links in the system.
-`)
-.addField(`Links`, `[OnlyCode](https://discord.gg/qVzp2cm)
-[Add to Server](https://discord.com/oauth2/authorize?client_id=727978641702649966&scope=bot&permissions=8)
-[Support Server](https://discord.gg/axczJaR)`)
-.setThumbnail(client.user.avatarURL)
-.setAuthor(`Uptime`, client.user.avatarURL)
-.setFooter(`2020 © Uptime | Coded by MertBhey, Edited by Alfonzo.`, client.user.avatarURL)
-return message.channel.send(embed);
-    }
- *
-})*/
-
-/*client.on("message", message => {
-  if(message.author.bot) return;
-    var spl = message.content.split(" ");
-  if(spl[0] == "u.help") {
-message.channel.send(`**Uptime Bot Commands v1.0**
-
-\`u.help\` - Displays the help menu.
-\`u.add\` - Adds the link you specified to the system.
-\`u.say\` - It shows the number of links in the system.
-
-2020 © Uptime | Coded by MertBhey, Edited by Alfonzo.
-`)
-  }
- 
-})*/
-
-/*client.on("message", async message => {
-
-  if(!message.content.startsWith("u.eval")) return;
-  if(!["689169122604744833","689169122604744833"].includes(message.author.id)) return;
-  var args = message.content.split("u.eval")[1]
-  if(!args) return message.channel.send("<:asuna_no:732219380795965471> ..")
-  
-      const code = args
-    
-    
-      function clean(text) {
-          if (typeof text !== 'string')
-              text = require('util').inspect(text, { depth: 3 })
-          text = text
-              .replace(/`/g, '`' + String.fromCharCode(8203))
-              .replace(/@/g, '@' + String.fromCharCode(8203))
-          return text;
-      };
-  
-      var evalEmbed = ""
-      try {
-          var evaled = await clean(await eval(await code));
-          if (evaled.constructor.name === 'Promise') evalEmbed = `\`\`\`\n${evaled}\n\`\`\``
-          else evalEmbed = `\`\`\`js\n${evaled}\n\`\`\``
-          
-  if(evaled.length < 1900) { 
-     message.channel.send(`\`\`\`js\n${evaled}\`\`\``);
-  } else {
-    var hast = await require("hastebin-gen")(evaled, { url: "https://hasteb.in" } )
-  message.channel.send(hast)
-  }
-      } catch (err) {
-          message.channel.send(`\`\`\`js\n${err}\n\`\`\``);
-      }
-  })
-  */
 const log = message => {
   console.log(`${message}`);
 };
